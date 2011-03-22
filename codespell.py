990a73f9f06f000408fde8c7b1707c16a09aa8c7
Jerry James <loganjerry@gmail.com>
2011-03-22 17:41:02 -0300

    Add license
    codespell is licensed as GPLv2

--- codespell.py
+++ codespell.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
 """
 Copyright (C) 2010 Lucas De Marchi <lucas.de.marchi@gmail.com>
 """
