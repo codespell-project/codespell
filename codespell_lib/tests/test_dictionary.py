fcf55d507cc5692beac49f9814e94f1e49e0353c
Peter Newman <peterjnewman@gmail.com>
2020-09-02 16:37:05 +0100

    Revert "Skip dedup->dedupe and dedupe->deduplicate for now"
    This reverts commit b70961a88e4d77f6df1573fd0a9fdf307eaa10d4.

--- codespell_lib/tests/test_dictionary.py
+++ codespell_lib/tests/test_dictionary.py
@@ -183,6 +183,7 @@ allowed_dups = {
     ('dictionary.txt', 'dictionary_rare.txt'),
     ('dictionary.txt', 'dictionary_usage.txt'),
     ('dictionary_rare.txt', 'dictionary_usage.txt'),
+    ('dictionary_rare.txt', 'dictionary_informal.txt'),
 }
 
 
