aded04f3b298aa3a7e96741e6f7ecb2a1e27d7c9
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2023-03-03 21:58:02 +0100

    Read config file without interpolation
    Apply 71907e2 / #2545 everywhere.

--- codespell_lib/_codespell.py
+++ codespell_lib/_codespell.py
@@ -571,7 +571,7 @@ def parse_options(
     # Collect which config files are going to be used
     used_cfg_files = []
     for cfg_file in cfg_files:
-        _cfg = configparser.ConfigParser()
+        _cfg = configparser.ConfigParser(interpolation=None)
         _cfg.read(cfg_file)
         if _cfg.has_section("codespell"):
             used_cfg_files.append(cfg_file)
