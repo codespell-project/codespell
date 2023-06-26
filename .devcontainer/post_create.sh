26ac0530748aaa3b9ed85a9daef2c04abeb2e561
Casey Korver <casey@korver.dev>
2023-06-26 13:57:11 +0000

    Add dev container config

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y libaspell-dev
+
+pip install --upgrade \
+    aspell-python-py3 \
+    pip \
+    setuptools \
+    setuptools_scm \
+    wheel
+pip install -e '.[dev]'
