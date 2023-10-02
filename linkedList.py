3e20c3f50f7c84a7ae91cea8b0086659f2fefc2f
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-10-02 13:43:23 +0530

    Create linkedList.py

---
+++ linkedList.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# Example usage:
+my_linked_list = LinkedList()
+my_linked_list.insert_at_beginning(1)
+my_linked_list.insert_at_beginning(2)
+my_linked_list.insert_at_end(3)
+my_linked_list.display()
