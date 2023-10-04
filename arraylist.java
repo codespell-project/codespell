2e46eeed5ce00f67060c7c3c75258681e5ea18ec
anshi-j <121630573+anshi-j@users.noreply.github.com>
2023-10-04 21:59:30 +0530

    Create arraylist.java

---
+++ arraylist.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
