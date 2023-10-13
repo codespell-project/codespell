5ffbb2e4428a5150656ce8751abf0634be00b423
samuel2345678998765345 <122578046+samuel2345678998765345@users.noreply.github.com>
2023-10-13 13:49:10 -0700

    Create game.py

---
+++ game.py
@@ -0,0 +1,19 @@
+import random
+
+def rock_paper_scissors():
+    choices = ['rock', 'paper', 'scissors']
+    computer_choice = random.choice(choices)
+    user_choice = input("Enter your choice (rock, paper, scissors): ")
+    
+    print(f"Computer chose {computer_choice}")
+    
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+         (user_choice == 'scissors' and computer_choice == 'paper') or \
+         (user_choice == 'paper' and computer_choice == 'rock'):
+        print("You win!")
+    else:
+        print("You lose!")
+
+rock_paper_scissors()
