a29444c2fda37ae34acc4d82d1e844c90e639fce
Shyam <140179838+MadMax1311@users.noreply.github.com>
2023-10-15 01:46:07 +0530

    Create Num.py

---
+++ Num.py
@@ -0,0 +1,22 @@
+# Python program using NumPy  
+# for some basic mathematical  
+# operations 
+  
+import numpy as np 
+  
+# Creating two arrays of rank 2 
+x = np.array([[1, 2], [3, 4]]) 
+y = np.array([[5, 6], [7, 8]]) 
+  
+# Creating two arrays of rank 1 
+v = np.array([9, 10]) 
+w = np.array([11, 12]) 
+  
+# Inner product of vectors 
+print(np.dot(v, w), "\n") 
+  
+# Matrix and Vector product 
+print(np.dot(x, v), "\n") 
+  
+# Matrix and matrix product 
+print(np.dot(x, y))
