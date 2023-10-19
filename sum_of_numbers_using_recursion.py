bbec84cd8d914ece0b6b787ea017491061d05ad6
Kavinkumar-2004 <146543755+Kavinkumar-2004@users.noreply.github.com>
2023-10-19 23:23:10 +0530

    sum_of_numbers_using_recursion.py

---
+++ sum_of_numbers_using_recursion.py
@@ -0,0 +1,9 @@
+def sum_of_digits(n):
+    if n < 10:
+        return n
+    else:
+        return n % 10 + sum_of_digits(n // 10)
+
+number = int(input("ENTER A NUMBER: "))
+result = sum_of_digits(number)
+print(f"The sum of digits in {number} is {result}")
