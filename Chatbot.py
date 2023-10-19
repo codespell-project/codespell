5330ac26b3c5a87075c7c4d58c7327968bc9ca7c
ABIR ROY <72157530+abhuman@users.noreply.github.com>
2023-10-20 02:29:14 +0530

    Create Chatbot.py

---
+++ Chatbot.py
@@ -0,0 +1,15 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Create a new instance of a ChatBot
+chatbot = ChatBot('Example Bot')
+
+# Create a new trainer for the chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Train the chatbot based on the english corpus
+trainer.train("chatterbot.corpus.english")
+
+# Get a response for some input text
+response = chatbot.get_response("Hello, how are you?")
+print(response)
