b741d26cd0c37fba186356459ce6c9e2b0895429
Kavinkumar-2004 <146543755+Kavinkumar-2004@users.noreply.github.com>
2023-10-19 23:09:17 +0530

    Create merge_sort_using_python.py

---
+++ merge_sort_using_python.py
@@ -0,0 +1,17 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    while left and right:
+        result.append((left if left[0] <= right[0] else right).pop(0))
+    return result + left + right
+
+arr = list(map(int, input("Enter the elements to sort :").split()))
+sorted_arr = merge_sort(arr)
+print(sorted_arr)
