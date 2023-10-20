6ed3d3ea5e02a891ac04edb7a0fbccedf89c6931
Kavinkumar-2004 <146543755+Kavinkumar-2004@users.noreply.github.com>
2023-10-20 21:25:09 +0530

    Delete decimal to hexadecimal.py

--- decimal to hexadecimal.py
+++
@@ -1,5 +0,0 @@
-decimal_number =int(input("Enter a decimal number to convert into hex: "))
-
-hexadecimal_number = hex(decimal_number)
-
-print(f"Decimal {decimal_number} is equivalent to Hexadecimal {hexadecimal_number}")
