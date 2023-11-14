aaaecc11b5570fd8eb8f85695add506e15ef1817
Jakub T. Jankiewicz <jcubic@onet.pl>
2023-11-14 21:26:44 +0100

    small refactor

--- codespell_lib/__main__.py
+++ codespell_lib/__main__.py
@@ -5,7 +5,5 @@ from ._codespell import _script_main
 if __name__ == "__main__":
     try:
         sys.exit(_script_main())
-    except BrokenPipeError:
-        pass
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, BrokenPipeError):
         pass
