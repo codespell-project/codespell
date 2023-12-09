1a40b3151a8058ac3dbb13f764a8906ae8610708
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2023-12-09 18:00:30 +0100

    Improve code coverage

--- codespell_lib/tests/test_basic.py
+++ codespell_lib/tests/test_basic.py
@@ -476,6 +476,7 @@ def test_ignore(
     assert cs.main("--skip=*ignoredir*", tmp_path) == 1
     assert cs.main("--skip=ignoredir", tmp_path) == 1
     assert cs.main("--skip=*ignoredir/bad*", tmp_path) == 1
+    assert cs.main(f"--skip={tmp_path}", tmp_path) == 0
     badjs = tmp_path / "bad.js"
     copyfile(badtxt, badjs)
     assert cs.main("--skip=*.js", goodtxt, badtxt, badjs) == 1
