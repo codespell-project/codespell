45390e2b0dfedcf7f1737092216f87a413fa49cd
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2024-01-19 21:09:36 +0100

    Fix ruff alert (currently) not caught by pre-commit
    PT007 Wrong values type in `@pytest.mark.parametrize` expected `list` of `tuple`

--- codespell_lib/tests/test_basic.py
+++ codespell_lib/tests/test_basic.py
@@ -1152,7 +1152,7 @@ def test_ill_formed_ini_config_file(
     assert "ill-formed config file" in stderr
 
 
-@pytest.mark.parametrize("kind", ("cfg", "toml", "toml_list"))
+@pytest.mark.parametrize("kind", ["cfg", "toml", "toml_list"])
 def test_config_toml(
     tmp_path: Path,
     capsys: pytest.CaptureFixture[str],
