ca2f00563114a1ec45605143d922d150186d3544
johann1764 <johann.brault_baron@yahoo.com>
2025-09-17 12:22:51 +0200

    added a bit of vocabulary gen_OX, en-GB_to_en-US

--- codespell_lib/gen_OX.sh
+++ codespell_lib/gen_OX.sh
@@ -11,12 +11,14 @@ SUFFIXES=(
 )
 SUFF=${SUFFIXES[*]}
 PAT='\(\('"${SUFF// /\\)\\|\\(}"'\)\)$'
-
+EXCEPT='^defenc'
 (
-   grep "$PAT" "$1"
+   grep "\($PAT\)\|\($EXCEPT\)" "$1"
    for i in e es ed ing ation ations ; do
-      echo "coloriz$i->colouriz$i"
-      echo "colouris$i->colouriz$i"
+      for j in col fav ; do
+         echo "${j}oriz$i->${j}ouriz$i"
+         echo "${j}ouris$i->${j}ouriz$i"
+      done
    done
    grep -v "$PAT" "$1" | sed 's/^\(.*\)->\(.*\)$/\2->\1/'
 ) | sort -d
