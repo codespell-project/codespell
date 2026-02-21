dc283f3bcd7fcd54c182ce18ee9ae4c7feee165e
vikivivi <22041592+vikivivi@users.noreply.github.com>
2026-02-21 21:19:21 +0800

    Fix case handling for various capitalization issues
    * Fix multi words capitalization, camelCase, proper nouns, abbreviation
    * Do not change suggested words in dictionary to lower case during build_dict()
    * Capitalization decision is decided in fix_case()

--- codespell_lib/_text_util.py
+++ codespell_lib/_text_util.py
@@ -17,11 +17,46 @@ Copyright (C) 2011  ProFUSION embedded systems
 """
 
 
+def is_camel_case_word(input_word: str) -> bool:
+    return (
+        (input_word != input_word.lower())
+        and (input_word != input_word.upper())
+        and ("_" not in input_word)
+        and ("-" not in input_word)
+        and (" " not in input_word)
+    )
+
+
+def is_camel_case_string(input_string: str) -> bool:
+    return any(is_camel_case_word(word) for word in input_string.split(","))
+
+
 def fix_case(word: str, fixword: str) -> str:
-    if word == word.capitalize():
-        return ", ".join(w.strip().capitalize() for w in fixword.split(","))
+    if fixword == fixword.upper():
+        # abbreviation, acronym: fixword is in all upper case.
+        # Use fixword as per dictionary.
+        # Eg. asscii->ASCII
+        return fixword
+    if word == word.capitalize() and fixword == fixword.lower():
+        # word is capitalized and fixword(s) in lower.
+        # Capitalize/Title fixword(s).
+        # Eg. Weather, Whether,
+        return fixword.title()
+    if word == word.capitalize() and not is_camel_case_string(fixword):
+        # word is capitalized and fixword(s) contain mixed with no camelCase.
+        # Capitalize/Title fixword(s).
+        # Eg. skipt->skip, Skype, skipped,
+        return fixword.title()
     if word == word.upper():
+        # word is in all upper case, change fixword to upper.
+        # Eg. MONDAY
         return fixword.upper()
-    # they are both lower case
-    # or we don't have any idea
+    if word.lower() == fixword.lower():
+        # Special feature only meant for private custom dictionary.
+        # word is valid but fixword required in CamelCase.
+        # Use fixword as per dictionary.
+        # Eg. mysql->MySQL
+        return fixword
+    # word is in lower, capitalize, CamelCase or whatever.
+    # Use fixword as per dictionary.
     return fixword
